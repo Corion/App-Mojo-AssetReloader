6c7fe0d2838a3157ba42e5ba01f054a77d1e6736
Max Maischein <corion@corion.net>
2018-04-27 23:26:09 +0200

    Fixup: Eliminate @watch

--- scripts/mojo-assetreloader.pl
+++ scripts/mojo-assetreloader.pl
@@ -238,7 +238,7 @@ unshift @{ app->static->paths }, @{ $config->{watch}};
 
 sub notify_changed( @files ) {
     my $config = app->config;
-    my $dir = $app->config->{watch}->[0]; # let's hope we only have one source for files for the moment
+    my $dir = app->config->{watch}->[0]; # let's hope we only have one source for files for the moment
 
     my @actions;
     for my $f (@files) {
