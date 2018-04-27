a510fa492d39bc75db04515ab400d9657fe67efd
Max Maischein <corion@corion.net>
2018-04-27 16:10:13 +0200

    Rough prototype of client-side hot-reloading in Mojolicious
    This needs server support because how would we send the notification
    to the client. Also, this should become a module so that any
    webserver can optionally hot-reload static (or generated) HTML.
    
    Preferrably, you would maybe have all of your templates trigger the
    hot reload too, at least in the "development" environment. I'm not so
    sure about modifying the HTML in production :)

--- mojolicious-hot-reload.pl
+++ mojolicious-hot-reload.pl
@@ -1,87 +1,11 @@
 #!perl -w
+package Reload::ChangeNotify::Threaded;
 use strict;
-use Mojolicious::Lite;
-use Path::Class 'dir';
-use Getopt::Long;
-
-GetOptions();
-
-my ($serve) = @ARGV;
-$serve ||= '.';
-
-my %inject = (
-    # Inject a live reload, keep all logic on the server
-    'text/html' => <<'HTML';
-<!-- hot-server appends this snippit to inject code via a websock  -->
-<script>
-new WebSocket(location.origin.replace(/^http/, 'ws')).onmessage = msg => {
-  var {path, type, str} = JSON.parse(msg.data)
-  if (type == 'reload') location.reload()
-  if (type == 'jsInject') eval(str)
-  if (type == 'cssInject') {
-    Array.from(document.querySelectorAll('link'))
-      .filter(d => d.href.includes(path))
-      .forEach(d => d.href = d.href)
-  }
-}
-</script>
-HTML
-);
-
-unshift @{ app->static->paths}, dir( $serve )->absolute;
-
-my @watch = glob '*';
-
-app->start;
-
-package Mojo::Server::Morbo::Backend::Win32;
-use strict;
-use Win32::ChangeNotify;
-use Win32::Event 'wait_any';
-use threads;
-use Mojo::Base 'Mojo::Server::Morbo::Backend';
-use Mojo::File 'path';
-
-sub _list { path(shift)->list_tree->map('to_string')->each }
-has _modified_files => sub { [] };
-has _cache => sub { {} };
-has _watchers => sub { [] };
-
-(my $html = $file) =~ s/\.pod$/\.html/i;
-
-sub modified_files( $self ) {
-    # Set up our watchers, just in case
-    $self->_modified_files
-}
-
-sub add_watch( $self, $path ) {
-    push @{ $self->_watchers }, Win32::ChangeNotify->new( $path, 1, "LAST_WRITE|SIZE" );
-}
-
-sub _on_change( $self ) {
-  
-  my @files = @{ $self->_modified_files };
-  my %files = map { $_ => 1 } @files;
-  for my $file (map { -f $_ && -r _ ? $_ : _list($_) } @{$self->watch}) {
-    my ($size, $mtime) = (stat $file)[7, 9];
-    next unless defined $size and defined $mtime;
-    my $stats = $cache->{$file} ||= [$^T, $size];
-    next if $mtime <= $stats->[0] && $size == $stats->[1];
-    @$stats = ($mtime, $size);
-    $files{ $file } = 1;
-  }
-  @{ $self->_modified_files } = sort keys %files;
-};
-
-1;
-
-package App::ShaderToy::FileWatcher;
-use strict;
-
 our $enabled;
 BEGIN {
-    eval "use threads;";
-    $enabled = !defined($@);
+    my $ok = eval { require threads; 1 };
+    my $err = $@;
+    $enabled = $ok;
 }
 use Thread::Queue;
 use Filesys::Notify::Simple;
@@ -92,13 +16,6 @@ use Filter::signatures;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
-=head1 NAME
-
-App::ShaderToy::FileWatcher - watch files for changes
-
-=cut
-
-# Launch our watcher thread for updates to the shader program:
 use vars qw($reload $watcher %watched_files);
 
 sub watch_files(@files) {
@@ -147,3 +64,109 @@ sub files_changed() {
 }
 
 1;
+
+package main;
+use strict;
+use Mojolicious::Lite;
+use Mojo::IOLoop;
+use Path::Class 'dir';
+use Getopt::Long;
+
+use Filter::signatures;
+use feature 'signatures';
+no warnings 'experimental::signatures';
+GetOptions();
+
+#my ($serve) = @ARGV;
+our $serve ||= '.';
+
+# Inject a live reload, keep all logic on the server
+my $inject = <<'HTML';
+<!-- hot-server appends this snippit to inject code via a websock  -->
+<script>
+new WebSocket(location.origin.replace(/^http/, 'ws')).onmessage = msg => {
+console.log(msg.data);
+  var {path, type, str} = JSON.parse(msg.data)
+  if (type == 'reload') location.reload()
+  if (type == 'jsInject') eval(str)
+  if (type == 'cssInject') {
+    Array.from(document.querySelectorAll('link'))
+      .filter(d => d.href.includes(path))
+      .forEach(d => d.href = d.href)
+  }
+}
+</script>
+HTML
+
+hook 'after_static' => sub( $c ) {
+    # serve everything as static
+    app->log->debug(sprintf "Serving static file '%s' (%s)", $c->req->url, $c->res->headers->content_type);
+    return if $c->res->headers->content_type !~ m!^text/html\b!i;
+    
+    app->log->debug(sprintf "Rewriting HTML for '%s'", $c->req->url);
+    
+    # rewrite HTML to append/add our <script> tag to the end
+    my $res = $c->res->content->asset->slurp;
+    $res =~ s!</body>!$inject</body>!i;
+    
+    my $r = Mojo::Asset::Memory->new();
+    $r->add_chunk( $res );
+    
+    # should we maybe remember an expected reconnect to our websocket here?!
+    # Also, before reloading, should we syntax-check things?!
+    $c->res->content->asset( $r );
+    
+    $res
+};
+
+our %pages;
+our $id = 0;
+websocket sub($c) {
+    #$c->tx->timeout( 3000 );
+    $pages{ $id++ } = $c->tx;
+    $c->inactivity_timeout(3600);
+    app->log->warn("Client $id connected");
+    $c->on(finish => sub( $c, @rest ) {
+        app->log->warn("Client $id disconnected");
+        delete $pages{ $id };
+    });
+};
+
+# Have a reload timer that will check
+my $dir = dir( $serve )->absolute;
+app->log->info("Watching things below $dir");
+unshift @{ app->static->paths }, $dir;
+
+# XXX
+my @watch = glob "$dir/*";
+#warn "[[@watch]]";
+
+sub notify_changed( @files ) {
+    for my $client (values %pages) {
+        app->log->warn("Notifying client of @files");
+        for my $f (@files) {
+            # Convert path to what the client will likely have requested (duh)
+            my $rel = Mojo::File->new($f);
+            $rel = $rel->to_rel( $dir );
+            $rel =~ s!\\!/!g;
+            if( $f =~ /\.html$/i ) {
+                $client->send({json => { path => $rel, type => 'reload', str => '' }});
+            } elsif( $f =~ /\.css/i ) {
+                $client->send({json => { path => $rel, type => 'cssInject', str => '' }});
+            } elsif( $f =~ /\.js/i ) {
+                my $content = Mojo::File->new( $f );
+                $client->send({json => { path => $rel, type => 'eval', str => $content->slurp }});
+            };
+        };
+    };
+}
+
+Reload::ChangeNotify::Threaded::watch_files( @watch );
+my $reload = Mojo::IOLoop->recurring(1, sub {
+    #warn "Checking";
+    my @changed = Reload::ChangeNotify::Threaded::files_changed();
+    app->log->debug("$_ changed") for @changed;
+    notify_changed(@changed) if @changed;
+});
+
+app->start;
